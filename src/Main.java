149d482fc3458b26c4955020b6650817fd172035
Nikita <Gammanik@gmail.com>
2015-12-20 00:06:54 +1000

    Token class added

--- src/Main.java
+++ src/Main.java
@@ -3,61 +3,99 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        String filelocation = "C:\\Users\\Nikita\\Dropbox\\git\\lexer\\files\\input.txt";
+        Lexer lex = new Lexer(filelocation);
 
-        Lexer lex = new Lexer("Let's look");
-        lex.reader = new BufferedReader(new FileReader(new File(lex.filelocation)));
         int i= 0;
-
         while (!lex.end) {
-            System.out.print(lex.NextSym());
+            lex.getToken();
+//            System.out.print(lex.NextSym());
+             if (lex.getToken() == null) System.out.println("sobaka(?)");
             i++;
         }
+//
+//        while (lex.getToken() != null) {
+//            System.out.println("sobaka(?)");
+//            Token token1 = lex.getToken();
+//            token1.printToken();
+//        }
 
         lex.reader.close();
-//        System.out.print('\r');
         System.out.println(i);
+
     }
 
 }
 
 
-class Lexer {
-    public String filelocation = "C:\\Users\\Nikita\\Dropbox\\git\\lexer\\files\\input.txt";
+class Lexer{
+//    public
     public BufferedReader reader = null;
     public boolean end = false;  //to check for the end of file
     public char ch;
 
-    String str;
-    public Lexer(String str) {this.str = str;}
+    public Lexer(String file) throws FileNotFoundException {this.reader = new BufferedReader(new FileReader(new File(file)));}
 
 
 
     char NextSym() throws IOException {
-
         int c= reader.read();  //int will become -1 when it'll reach the end of file
         ch = (char)c;
 
 //        if (ch == '\n')  //for future line and column count
 //        if (ch == '\t')
 //        if (ch == '\r')
-
         if (c == -1){ //check for the EOF and mark it
             end = true;
-            ch = '@';
+            ch = '~';
         }
-        SkipWhiteSpaces();
         return ch;
     }
 
     void SkipWhiteSpaces() throws IOException { //called when ve have recognized a token
-        if (ch == '\r') NextSym();
+        if (ch == '\r' || ch == '\n') NextSym();
     }
 
-    void getToken()//will return the Token
+    Token getToken() throws IOException//will return the exact Token
     {
-        /*
-        /a lot of if-else things/
-         */
+        NextSym();
+        SkipWhiteSpaces();
+        if (ch == '~'){System.out.println("eeert"); return null; }
+
+        int line = 0, column = 0;
+        String type = "", lexeme = "";
+        if (Character.isLetter(ch)){
+            lexeme = lexeme + Character.toString(ch);
+            NextSym();
+           while  (Character.isDigit(ch) || Character.isLetter(ch)) { // write if there(?)
+                type = "ident";
+                lexeme = lexeme + Character.toString(ch);
+                NextSym();
+            }
+        }
+
+
+        return new Token(line, column, type, lexeme);
     }
+
+}
+
+
+class Token{
+
+
+    int line, column;
+    String type, lexeme, value;
+    public Token(int line, int column, String type, String lexeme){
+        this.line = line;
+        this.column = column;
+        this.type = type;
+        this.lexeme = lexeme;
+
+    }
+
+   void printToken(){
+       System.out.println(line+" "+column+" "+type+" "+lexeme);
+   }
 
 }
